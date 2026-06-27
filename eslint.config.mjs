ad3a1fbaee186041841b7a4f4e175dcae2ab8e3f
Andrew Thompson <Aj.thompson8888@gmail.com>
2026-06-27 11:58:37 -0400

    chore: configure ESLint for monorepo

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import js from "@eslint/js";
+import typescript from "typescript-eslint";
+
+export default [
+    js.configs.recommended,
+    ...typescript.configs.recommended,
+{
+    ignores: [
+        "node_modules/",
+        "dist/",
+        "build",
+        "coverage",
+    ]
+}
+];
