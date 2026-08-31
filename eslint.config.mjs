534e2cbc0afd9e17f7b03f635d2a2547a3b62ddb
Andrew Thompson <Aj.thompson8888@gmail.com>
2026-08-31 11:04:43 -0400

    chore: improve Docker Prisma and ESLint tooling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,17 @@ import js from "@eslint/js";
 import typescript from "typescript-eslint";
 
 export default [
-    js.configs.recommended,
-    ...typescript.configs.recommended,
     {
-        ignores: ["node_modules/", "dist/", "build", "coverage"],
+        ignores: [
+            "**/node_modules/**",
+            "**/dist/**",
+            "**/build/**",
+            "**/coverage/**",
+            "generated/**",
+        ],
     },
+
+    js.configs.recommended,
+
+    ...typescript.configs.recommended,
 ];
